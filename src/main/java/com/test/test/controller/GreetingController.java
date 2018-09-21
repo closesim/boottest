b5a0519c3ee32a025caa0d4d2238510eabf2f313
Miguel Borja <miguel.borja@i2btech.com>
2018-09-21 16:15:31 -0500

    Agregado Thymeleaf

--- src/main/java/com/test/test/controller/GreetingController.java
+++ src/main/java/com/test/test/controller/GreetingController.java
@@ -1,13 +1,12 @@
 package com.test.test.controller;
 
+import com.test.test.domain.Greeting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.test.test.domain.Greeting;
 
 /**
  *  Controlador de prueba
