a5b142ddc34ba902f77b132f9b955f50fde738fc
Miguel Borja <miguel.borja@i2btech.com>
2018-09-24 11:35:24 -0500

    Agregado Spring Security

--- src/main/java/com/test/test/controller/ViewController.java
+++ src/main/java/com/test/test/controller/ViewController.java
@@ -7,9 +7,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ViewController {
+
     @GetMapping("/test")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "test";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
     }
 }
